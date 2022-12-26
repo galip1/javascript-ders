761e96541d737728019f1f2021c81e9f8f0dbb5c
galip <galipts1@gmail.com>
2022-12-26 07:01:50 +0300

    25.12.22

--- 16.gun/modules.js
+++ 16.gun/modules.js
@@ -1,4 +1,6 @@
 import showAlert, { showConfirm } from "../assets/js/modules/message.js";
+//export default showAlert; //bir tane default tanımlanır
+//ön tarafa eklenir default olarak tanımlanırsa
 
 document.getElementById("btn1").addEventListener("click", () => {
   showAlert("hello js");
